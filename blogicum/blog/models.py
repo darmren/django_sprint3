ea14126b66d4cd6f4328b39688ef3984192fb04d
darmren <daramstfn@yandex.ru>
2025-11-04 16:45:34 +0700

    pep fixes

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -34,7 +34,8 @@ class Category(PublishedModel):
     slug = models.SlugField(
         unique=True,
         verbose_name='Идентификатор',
-        help_text='Идентификатор страницы для URL; разрешены символы латиницы, цифры, дефис и подчёркивание.'
+        help_text='Идентификатор страницы для URL; \
+        разрешены символы латиницы, цифры, дефис и подчёркивание.'
     )
 
     class Meta:
@@ -73,7 +74,8 @@ class Post(PublishedModel):
     )
     pub_date = models.DateTimeField(
         verbose_name='Дата и время публикации',
-        help_text='Если установить дату и время в будущем — можно делать отложенные публикации.')
+        help_text='Если установить дату и время в будущем — \
+        можно делать отложенные публикации.')
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
